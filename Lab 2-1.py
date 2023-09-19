0655e83f9334298d7964437c1591d936436aef73
JNeaderland <144148081+JNeaderland@users.noreply.github.com>
2023-09-19 13:36:43 -0400

    Add files via upload

---
+++ Lab 2-1.py
@@ -0,0 +1,23 @@
+#Author: Jacob Neaderland
+x = 123 + 321 - 321
+y = 20+ 202020 - 420 + 20
+z = 2 * 4 * 6 * 8
+a = 9 / 5
+b = 9 // 5
+c = 13/0
+d = 3 % 10
+e = 3 % 10
+f = 2 ** 3		
+g = 2 * 2 * 2
+h = 0/0
+print (x)
+print (y)
+print (z)
+print (a)
+print (b)
+print (c)
+print (d)
+print (e)
+print (f)
+print (g)
+print (h)
---
+++ Lab 2-2.py
@@ -0,0 +1,10 @@
+#Author: Jacob Neaderland
+b = 3
+a = b
+print (a)
+print (b)
+# a and b are both 3 because I said that b is 3 and a is b
+a = 7
+print (a)
+print (b)
+# b is still 3 a is now 7 because I didn't change b but set a to 7
